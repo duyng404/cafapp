e0cbe734b28e9e0c4bd2ede81901c1cf02800e43
ferb96 <fduy96@gmail.com>
2018-04-07 21:45:17 -0500

    testing CAS connection

--- app/CafApp.py
+++ app/CafApp.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
-from app import app
+from app import app, cas
+from flask_cas import login_required
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
-
+@login_required
+def default():
+    print('someone access this page')
+    print('dir=',dir(cas))
+    print('vars:')
+    attrs = vars(cas)
+    print(', '.join("%s: %s" % item for item in attrs.items()))
+    return render_template('index.html',username=cas.username)
--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,22 @@
 from flask import Flask
+from flask_cas import CAS
+from flask_cas import login_required
+#from flask.ext.cas import CAS
+#from flask.ext.cas import login_required
 
 # Initialize the app
 app = Flask(__name__)
+
+# CAS client config
+cas = CAS(app)
+app.config['CAS_SERVER'] = 'https://sso.gac.edu'
+app.config['CAS_LOGIN_ROUTE'] = '/idp/profile/cas/login'
+app.config['CAS_LOGOUT_ROUTE'] = '/idp/profile/cas/logout'
+app.config['CAS_VALIDATE_ROUTE'] = '/cas/serviceValidate'
+app.config['CAS_AFTER_LOGIN'] = 'default'
+
 app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 's3cr3t'
 
 # Everything else is in the file CafApp.py
 from app import CafApp
